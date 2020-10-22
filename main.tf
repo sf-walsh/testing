03574ef298edc7fb4509833f209b14c25107384a
sf-walsh <sf.walsh@gmail.com>
2020-10-22 11:50:30 -0400

    Simple Web Server with busybox and security group

--- main.tf
+++ main.tf
@@ -3,11 +3,28 @@ provider "aws" {
 }
 
 resource "aws_instance" "test" {
-    ami = "ami-0eb97cc600df1a8ca"
+    ami = "ami-0dba2cb6798deb6d8"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, world" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
 
     tags = {
         Name = "terraform-example"
     }
 }
 
+resource "aws_security_group" "instance" {
+    name = "terraform-test-instance"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
